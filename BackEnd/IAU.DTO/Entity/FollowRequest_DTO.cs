e8f76abf4204aa14f35815742ee569698d6c12fd
RamyMakram <ramymakrameyd@outlook.com>
2021-04-21 16:43:06 +0200

    delete auto mapper

---
+++ BackEnd/IAU.DTO/Entity/FollowRequest_DTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace IAU.DTO.Entity
+{
+	public class FollowRequest_DTO
+	{
+		public byte statusId;
+
+		public string requestCode { get; set; }
+		public int requestid { get; set; } = 0;
+		public string location { get; set; }
+		public string status { get; set; }
+		public DateTime deliverydate { get; set; }
+	}
+}
