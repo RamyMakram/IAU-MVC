5f0f4036237fd813291990160a4112c9736d9b28
RamyMakram <ramymakrameyd@outlook.com>
2021-04-27 12:56:15 +0200

    web socket

---
+++ BackEnd/IAUBackEnd.Admin/Controllers/MyServiceController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.WebSockets;
+
+namespace IAUBackEnd.Admin.Controllers
+{
+	class MyServiceController : ApiController
+	{
+		public HttpResponseMessage Get(string param)
+		{
+			HttpContext currentContext = HttpContext.Current;
+			if (currentContext.IsWebSocketRequest ||
+				currentContext.IsWebSocketRequestUpgrading)
+			{
+				currentContext.AcceptWebSocketRequest(ProcessWebsocketSession);
+				return Request.CreateResponse(HttpStatusCode.SwitchingProtocols);
+			}
+			return Request.CreateResponse(HttpStatusCode.BadRequest);
+		}
+
+		private async Task ProcessWebsocketSession(AspNetWebSocketContext context)
+		{
+			var ws = context.WebSocket;
+
+			new Task(async () =>
+			{
+				var inputSegment = new ArraySegment<byte>(new byte[1024]);
+
+				while (true)
+				{
+					// MUST read if we want the state to get updated...
+					var result = await ws.ReceiveAsync(inputSegment, CancellationToken.None);
+
+					if (ws.State != WebSocketState.Open)
+					{
+						break;
+					}
+				}
+			}).Start();
+
+			while (true)
+			{
+				if (ws.State != WebSocketState.Open)
+				{
+					break;
+				}
+				else
+				{
+					byte[] binaryData = { 0xde, 0xad, 0xbe, 0xef, 0xca, 0xfe };
+					var segment = new ArraySegment<byte>(binaryData);
+					await ws.SendAsync(segment, WebSocketMessageType.Binary,
+						true, CancellationToken.None);
+				}
+			}
+		}
+	}
+}
---
+++ BackEnd/IAUBackEnd.Admin/MicrosoftWebsocket.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using Microsoft.Web.WebSockets;
+namespace IAUBackEnd.Admin
+{
+	public class WebSocketManager : WebSocketHandler
+	{
+		private static WebSocketCollection clients = new WebSocketCollection();
+		private static Dictionary<string, WebSocketCollection> Mapped = new Dictionary<string, WebSocketCollection>();
+		public override void OnOpen()
+		{
+			string name = this.WebSocketContext.QueryString["Name"];
+			if (Mapped.ContainsKey(name))
+				Mapped[name].Add(this);
+			else
+				Mapped[name] = new WebSocketCollection() { this };
+			clients.Add(this);
+		}
+		public static void SendTo(string Name, string Message)
+		{
+			Mapped[Name].Broadcast(Message);
+		}
+
+		public override void OnMessage(byte[] message)
+		{
+			//clients.Broadcast(string.Format("{0} said: {1}", name, message));
+		}
+		public override void OnClose()
+		{
+			clients.Remove(this);
+			var WillBeRemoved = Mapped.Where(q => q.Value.Contains(this)).Select(q => q.Key);
+			foreach (var i in WillBeRemoved)
+				Mapped[i].Remove(this);
+		}
+	}
+}
