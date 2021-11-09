9ff51cc4c2f6fa7b906b1bba7f8a55a4ddcffb7a
Ramy Makram <RamyMakramEyd@outlook.com>
2021-11-09 13:52:24 +0200

    edit language typing error

--- FrontEnd/AdminPanel/Global.asax.cs
+++ FrontEnd/AdminPanel/Global.asax.cs
@@ -22,50 +22,50 @@ namespace AdminPanel
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             MvcHandler.DisableMvcResponseHeader = true;
-            Task.Run(async () =>
-            {
-                do
-                {
-                    using (var socket = new ClientWebSocket())
-                        try
-                        {
-                            await socket.ConnectAsync(new Uri("wss://localhost:44344/WSHandler.ashx?Name=35"), CancellationToken.None);
-                            await Receive(socket);
+            //Task.Run(async () =>
+            //{
+            //    do
+            //    {
+            //        using (var socket = new ClientWebSocket())
+            //            try
+            //            {
+            //                await socket.ConnectAsync(new Uri("wss://localhost:44344/WSHandler.ashx?Name=35"), CancellationToken.None);
+            //                await Receive(socket);
 
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine($"ERROR - {ex.Message}");
-                        }
-                } while (true);
-            });
+            //            }
+            //            catch (Exception ex)
+            //            {
+            //                Console.WriteLine($"ERROR - {ex.Message}");
+            //            }
+            //    } while (true);
+            //});
         }
-        static async Task Receive(ClientWebSocket socket)
-        {
-            var buffer = new ArraySegment<byte>(new byte[2048]);
-            do
-            {
-                WebSocketReceiveResult result;
-                using (var ms = new MemoryStream())
-                {
-                    do
-                    {
-                        result = await socket.ReceiveAsync(buffer, CancellationToken.None);
-                        ms.Write(buffer.Array, buffer.Offset, result.Count);
-                    } while (!result.EndOfMessage);
+        //static async Task Receive(ClientWebSocket socket)
+        //{
+        //    var buffer = new ArraySegment<byte>(new byte[2048]);
+        //    do
+        //    {
+        //        WebSocketReceiveResult result;
+        //        using (var ms = new MemoryStream())
+        //        {
+        //            do
+        //            {
+        //                result = await socket.ReceiveAsync(buffer, CancellationToken.None);
+        //                ms.Write(buffer.Array, buffer.Offset, result.Count);
+        //            } while (!result.EndOfMessage);
 
-                    if (result.MessageType == WebSocketMessageType.Close)
-                        break;
+        //            if (result.MessageType == WebSocketMessageType.Close)
+        //                break;
 
-                    ms.Seek(0, SeekOrigin.Begin);
-                    using (var reader = new StreamReader(ms, Encoding.UTF8))
-                    {
-                        var data = await reader.ReadToEndAsync();
-                        Console.WriteLine(data);
-                    }
-                }
-            } while (true);
-        }
+        //            ms.Seek(0, SeekOrigin.Begin);
+        //            using (var reader = new StreamReader(ms, Encoding.UTF8))
+        //            {
+        //                var data = await reader.ReadToEndAsync();
+        //                Console.WriteLine(data);
+        //            }
+        //        }
+        //    } while (true);
+        //}
         protected void Application_Error()
         {
             HttpContext.Current.Response.Redirect(Request.Url.ToString().Replace(Request.Url.PathAndQuery, "") + "/" + "Error");
