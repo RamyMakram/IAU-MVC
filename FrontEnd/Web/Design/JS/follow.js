0228056f536a481c6c94fb48d5f3d5e1ae2ffb3b
Ramy Makram <RamyMakramEyd@outlook.com>
2022-03-02 14:09:08 +0200

    change mustafid or twasul

--- FrontEnd/Web/Design/JS/follow.js
+++ FrontEnd/Web/Design/JS/follow.js
@@ -1,109 +1,110 @@
 ﻿var language = GetCookie();
 
 function GetCookie() {
-	let name = "lang=";
-	let cookieValue = null;
-	var decodedCookie = decodeURIComponent(document.cookie);
-	var ca = decodedCookie.split(';');
-	for (var i = 0; i < ca.length; i++) {
-		var c = ca[i];
-		while (c.charAt(0) == ' ') {
-			c = c.substring(1);
-		}
-		if (c.indexOf(name) == 0) {
-			cookieValue = c.substring(name.length, c.length);
-		}
-	}
-	return cookieValue;
+    let name = "lang=";
+    let cookieValue = null;
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            cookieValue = c.substring(name.length, c.length);
+        }
+    }
+    return cookieValue;
 }
 
 $(".containt2 input.search").click(function (element) {
-	if (!$(element.currentTarget).val()) {
-		$(".containt2 input.search").each(element => {
-			if (!$(".containt2 input.search:eq(" + element + ")").val()) {
-				$(".containt2 input.search:eq(" + element + ")").focus();
-				return false;
-			}
-		});
-	}
+    if (!$(element.currentTarget).val()) {
+        $(".containt2 input.search").each(element => {
+            if (!$(".containt2 input.search:eq(" + element + ")").val()) {
+                $(".containt2 input.search:eq(" + element + ")").focus();
+                return false;
+            }
+        });
+    }
 });
 let inputLocatin;
 $(".containt2 input.search").keyup(function (element) {
 
-	$(".containt2 input.search").each(elementCount => {
-		if (!$(".containt2 input.search:eq(" + elementCount + ")").val()) {
-			$(".containt2 input.search:eq(" + elementCount + ")").focus();
-			inputLocatin = elementCount;
-			return false;
-		}
-	});
+    $(".containt2 input.search").each(elementCount => {
+        if (!$(".containt2 input.search:eq(" + elementCount + ")").val()) {
+            $(".containt2 input.search:eq(" + elementCount + ")").focus();
+            inputLocatin = elementCount;
+            return false;
+        }
+    });
 
-	if (element.key == "Backspace") {
-		if (inputLocatin > 0) {
-			$(".containt2 input.search:eq(" + inputLocatin-- + ")").val("");
-			$(".containt2 input.search:eq(" + inputLocatin-- + ")").focus();
-		}
-	}
+    if (element.key == "Backspace") {
+        if (inputLocatin > 0) {
+            $(".containt2 input.search:eq(" + inputLocatin-- + ")").val("");
+            $(".containt2 input.search:eq(" + inputLocatin-- + ")").focus();
+        }
+    }
 
-	if (element.key == "Enter") {
-		show();
-	}
+    if (element.key == "Enter") {
+        show();
+    }
 });
 
 function showState() {
-	show();
+    show();
 }
 
 function searchByPhone() {
-	var myModal = new bootstrap.Modal(document.getElementById('phoneSearch'), {
-		keyboard: false
-	})
-	myModal.show()
+    var myModal = new bootstrap.Modal(document.getElementById('phoneSearch'), {
+        keyboard: false
+    })
+    myModal.show()
 }
 
 function show() {
-	var requestCode = "";
-	$(".containt2 input.search").each(element => {
-		requestCode = requestCode + $(".containt2 input.search:eq(" + element + ")").val();
-	});
-	if (requestCode.trim().length == 13) {
-		$.ajax({
-			url: "/Follow/FollowRequest", data: {
-				__RequestVerificationToken: $('input[name="__RequestVerificationToken"]').val(),
-				requestCode
-			},
+    var requestCode = "";
+    $(".containt2 input.search").each(element => {
+        requestCode = requestCode + $(".containt2 input.search:eq(" + element + ")").val();
+    });
+    if (requestCode.trim().length == 13) {
+        $.ajax({
+            url: "/Follow/FollowRequest", data: {
+                __RequestVerificationToken: $('input[name="__RequestVerificationToken"]').val(),
+                requestCode
+            },
             method: "Post",
-			success: function (result) {
-				let data = JSON.parse(result);
-				if (data.success) {
-					if (data.result == null) {
-						alert(language == "ar" ? "!كود خاطئ" : "Wrong Code!")
-						$("#div_Follow").css("display", "none");
-					}
-					else {
-						$(".containt2 .row .col-md-12:nth-of-type(3)").css("display", "block");
-						let isAr = $('#lang').text() == "ar"
-						let request = data.result;
-						$('.state').removeClass('progress-sucess-state')
-						$('.state-' + request.Request.Request_State_ID).addClass('progress-sucess-state')
-						for (var i = 1; i < request.Request.Request_State_ID; i++) {
-							$('.state-' + i).addClass('progress-sucess-state')
-						}
+            success: function (result) {
+                let data = JSON.parse(result);
+                if (data.success) {
+                    if (data.result == null) {
+                        alert(language == "ar" ? "!كود خاطئ" : "Wrong Code!")
+                        $("#div_Follow").css("display", "none");
+                    }
+                    else {
+                        $(".containt2 .row .col-md-12:nth-of-type(3)").css("display", "block");
+                        let isAr = $('#lang').text() == "ar"
+                        let request = data.result;
+                        $('.state').removeClass('progress-sucess-state')
+                        $('.state-' + request.Request.Request_State_ID).addClass('progress-sucess-state')
+                        for (var i = 1; i < request.Request.Request_State_ID; i++) {
+                            $('.state-' + i).addClass('progress-sucess-state')
+                        }
 
-						$('#spn_location').text((isAr ? request.State.Units.Units_Name_AR : request.State.Units.Units_Name_EN).slice(0, 12))
-						$('#spn_location').attr('title', (isAr ? request.State.Units.Units_Name_AR : request.State.Units.Units_Name_EN))
-						$('#spn_status').text((isAr ? request.Request.Request_State.StateName_AR : request.Request.Request_State.StateName_EN).slice(0, 12))
-						$('#spn_status').attr('title', isAr ? request.Request.Request_State.StateName_AR : request.Request.Request_State.StateName_EN)
-						$('#spn_deliverydate').text(new Date(request.State.ExpireDays).toDateString())
+                        $('#spn_shipped').text((isAr ? (request.Request.IsTwasul_OC ? "تواصل" : "مستفيد") : (request.Request.IsTwasul_OC ? "Twasul" : "Mustafid")))
+                        $('#spn_location').text((isAr ? request.State.Units.Units_Name_AR : request.State.Units.Units_Name_EN).slice(0, 12))
+                        $('#spn_location').attr('title', (isAr ? request.State.Units.Units_Name_AR : request.State.Units.Units_Name_EN))
+                        $('#spn_status').text((isAr ? request.Request.Request_State.StateName_AR : request.Request.Request_State.StateName_EN).slice(0, 12))
+                        $('#spn_status').attr('title', isAr ? request.Request.Request_State.StateName_AR : request.Request.Request_State.StateName_EN)
+                        $('#spn_deliverydate').text(new Date(request.State.ExpireDays).toDateString())
 
-						$("#div_Follow").css("display", "block");
-					}
-				}
-				else {
-					alert(language == "ar" ? "!كود خاطئ" : "Wrong Code!")
+                        $("#div_Follow").css("display", "block");
+                    }
+                }
+                else {
+                    alert(language == "ar" ? "!كود خاطئ" : "Wrong Code!")
 
-				}
-			}
-		});
-	}
+                }
+            }
+        });
+    }
 }
