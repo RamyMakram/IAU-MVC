678b84c170655e8b7958702ed1e7d6a8e35f994c
Ramy Makram <RamyMakramEyd@outlook.com>
2022-03-07 13:07:07 +0200

    handel location names in home page

--- BackEnd/IAUBackEnd.Admin/Controllers/StatisticsController.cs
+++ BackEnd/IAUBackEnd.Admin/Controllers/StatisticsController.cs
@@ -17,7 +17,7 @@ namespace IAUBackEnd.Admin.Controllers
 		{
 			var MostafidCount = p.Service_Type.Select(q => new { AR = q.Service_Type_Name_AR, EN = q.Service_Type_Name_EN, IMG = q.Image_Path, Count = q.Request_Data.Count() }).ToList();
 			var Stat = new { Units = p.Units.Count(), Requests = p.Request_Data.Count(), MainServices = p.Main_Services.Count(), SubServices = p.Sub_Services.Count() };
-			var Locations = p.Units_Location.Select(q => new { AR = q.Units_Location_Name_AR, EN = q.Units_Location_Name_EN, Count = (int?)q.Units.Sum(w => w.Request_Data.Count) ?? 0 }).OrderByDescending(q => q.Count).Take(4).ToList();
+			var Locations = p.Units_Location.Select(q => new { AR = q.Units_Location_Name_AR, EN = q.Units_Location_Name_EN, Count = (int?)q.Units.Sum(w => w.Request_Data.Count) ?? 0 }).ToList();
 			return Ok(new ResponseClass() { success = true, result = new { MostafidCount, Stat, Locations } });
 		}
 	}
