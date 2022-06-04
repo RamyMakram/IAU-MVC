b72ef3ef9e5c486682eea11e89fae114f25c73ee
Ramy Makram <RamyMakramEyd@outlook.com>
2022-06-04 10:11:40 +0200

    Finish UnitLocation Restore

--- BackEnd/IAUAdmin.DTO/Entity/UnitsLocDTO.cs
+++ BackEnd/IAUAdmin.DTO/Entity/UnitsLocDTO.cs
@@ -6,14 +6,15 @@ using System.Threading.Tasks;
 
 namespace IAUAdmin.DTO.Entity
 {
-	public class UnitsLocDTO
-	{
+    public class UnitsLocDTO
+    {
         public int Units_Location_ID { get; set; }
         public string Units_Location_Name_AR { get; set; }
         public string Units_Location_Name_EN { get; set; }
         public string Name { get; set; }
         public Nullable<int> Location_ID { get; set; }
         public Nullable<bool> IS_Action { get; set; }
+        public Nullable<DateTime> DeletedAt { get; set; }
 
         public virtual LocationsDTO Location { get; set; }
         //public virtual ICollection<Units> Units { get; set; }
