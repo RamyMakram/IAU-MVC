a2f11b137ee54bb2ac4e36c983c5f1f9b2ad8b38
Ramy Makram <RamyMakramEyd@outlook.com>
2022-06-04 10:17:43 +0200

    Finish SubService Restore

--- BackEnd/IAUAdmin.DTO/Entity/SubServicesDTO.cs
+++ BackEnd/IAUAdmin.DTO/Entity/SubServicesDTO.cs
@@ -13,6 +13,7 @@ namespace IAUAdmin.DTO.Entity
 		public string Sub_Services_Name_AR { get; set; }
 		public Nullable<int> Main_Services_ID { get; set; }
 		public Nullable<bool> IS_Action { get; set; }
+		public Nullable<DateTime> DeletedAt { get; set; }
 		public string Required { get; set; }
 		public ICollection<RequiredDocsDTO> Required_Documents { get; set; }
 		public virtual MainServiceDTO Main_Services { get; set; }
