62fc7fa4b119837e8da24e09c421865fa3809437
Ramy Makram <RamyMakramEyd@outlook.com>
2022-06-04 10:24:57 +0200

    Finish ServiceType Restore

--- BackEnd/IAUAdmin.DTO/Entity/ServiceTypeDTO.cs
+++ BackEnd/IAUAdmin.DTO/Entity/ServiceTypeDTO.cs
@@ -7,15 +7,16 @@ using System.Web;
 
 namespace IAUAdmin.DTO.Entity
 {
-	public class ServiceTypeDTO
-	{
-		public int Service_Type_ID { get; set; }
-		public string Service_Type_Name_EN { get; set; }
-		public string Service_Type_Name_AR { get; set; }
-		public string Name { get; set; }
-		public Nullable<bool> IS_Action { get; set; }
-		public string Image_Path { get; set; }
-		public string Base64 { get; set; }
-		public List<HttpPostedFileBase> Files { get; set; }
-	}
+    public class ServiceTypeDTO
+    {
+        public int Service_Type_ID { get; set; }
+        public string Service_Type_Name_EN { get; set; }
+        public string Service_Type_Name_AR { get; set; }
+        public string Name { get; set; }
+        public Nullable<bool> IS_Action { get; set; }
+        public Nullable<DateTime> DeletedAt { get; set; }
+        public string Image_Path { get; set; }
+        public string Base64 { get; set; }
+        public List<HttpPostedFileBase> Files { get; set; }
+    }
 }
