64343c8d31a73597d0e54dae0bdef9c874a65bb3
Ramy Makram <RamyMakramEyd@outlook.com>
2022-06-07 11:29:11 +0200

    Location without test

--- BackEnd/IAUBackEnd.Admin/Controllers/LocationsController.cs
+++ BackEnd/IAUBackEnd.Admin/Controllers/LocationsController.cs
@@ -11,24 +11,25 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using IAUAdmin.DTO.Helper;
 using IAUBackEnd.Admin.Models;
+using Newtonsoft.Json;
 
 namespace IAUBackEnd.Admin.Controllers
 {
 	public class LocationsController : ApiController
 	{
-		private MostafidDBEntities p = new MostafidDBEntities();
+		private MostafidDBEntities db = new MostafidDBEntities();
 
 		public async Task<IHttpActionResult> GetLocations()
 		{
-			return Ok(new ResponseClass() { success = true, result = p.Location });
+			return Ok(new ResponseClass() { success = true, result = db.Location });
 		}
 		public async Task<IHttpActionResult> GetActive()
 		{
-			return Ok(new ResponseClass() { success = true, result = p.Location.Where(q => q.IS_Action.Value) });
+			return Ok(new ResponseClass() { success = true, result = db.Location.Where(q => q.IS_Action.Value) });
 		}
 		public async Task<IHttpActionResult> GetLocation(int id)
 		{
-			Location location = await p.Location.FindAsync(id);
+			Location location = await db.Location.FindAsync(id);
 			if (location == null)
 				return Ok(new ResponseClass() { success = false, result = "Location Is Null" });
 
@@ -41,13 +42,31 @@ namespace IAUBackEnd.Admin.Controllers
 		{
 			try
 			{
-				var data = p.Location.FirstOrDefault(q => q.Location_ID == location.Location_ID);
+				var data = db.Location.FirstOrDefault(q => q.Location_ID == location.Location_ID);
 				if (!ModelState.IsValid || data == null)
 					return Ok(new ResponseClass() { success = false, result = ModelState });
+				var OldVals = JsonConvert.SerializeObject(data, new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore });
+
+				var trans = db.Database.BeginTransaction();
+
 				data.Location_Name_AR = location.Location_Name_AR;
 				data.Location_Name_EN = location.Location_Name_EN;
-				await p.SaveChangesAsync();
-				return Ok(new ResponseClass() { success = true });
+				await db.SaveChangesAsync();
+				var logstate = Logger.AddLog(db: db, logClass: LogClassType.Locations, Method: "Update", Oldval: OldVals, Newval: data, es: out _, syslog: out _, ID: data.Location_ID, notes: "");
+				if (logstate)
+				{
+					await db.SaveChangesAsync();
+					trans.Commit();
+					return Ok(new ResponseClass()
+					{
+						success = true
+					});
+				}
+				else
+				{
+					trans.Rollback();
+					return Ok(new ResponseClass() { success = false });
+				}
 			}
 			catch (Exception ee)
 			{
@@ -59,44 +78,96 @@ namespace IAUBackEnd.Admin.Controllers
 		{
 			if (!ModelState.IsValid)
 				return Ok(new ResponseClass() { success = false });
-			location.IS_Action = true;
-			p.Location.Add(location);
-			await p.SaveChangesAsync();
+			var trans = db.Database.BeginTransaction();
 
-			return Ok(new ResponseClass() { success = true });
+			location.IS_Action = true;
+			db.Location.Add(location);
+			await db.SaveChangesAsync();
+
+			var logstate = Logger.AddLog(db: db, logClass: LogClassType.Locations, Method: "Update", Oldval: null, Newval: location, es: out _, syslog: out _, ID: location.Location_ID, notes: "");
+			if (logstate)
+			{
+				await db.SaveChangesAsync();
+				trans.Commit();
+				return Ok(new ResponseClass()
+				{
+					success = true
+				});
+			}
+			else
+			{
+				trans.Rollback();
+				return Ok(new ResponseClass() { success = false });
+			}
 		}
 		[HttpGet]
 		public async Task<IHttpActionResult> DeactiveLocation(int id)
 		{
-			Location location = await p.Location.FindAsync(id);
+			Location location = await db.Location.FindAsync(id);
 			if (location == null)
 			{
 				return NotFound();
 			}
-			location.IS_Action = false;
-			await p.SaveChangesAsync();
+			var trans = db.Database.BeginTransaction();
 
-			return Ok(new ResponseClass() { success = true });
+			location.IS_Action = false;
+
+			await db.SaveChangesAsync();
+
+
+			var logstate = Logger.AddLog(db: db, logClass: LogClassType.Locations, Method: "Deactive", Oldval: null, Newval: null, es: out _, syslog: out _, ID: location.Location_ID, notes: "");
+			if (logstate)
+			{
+				await db.SaveChangesAsync();
+				trans.Commit();
+				return Ok(new ResponseClass()
+				{
+					success = true
+				});
+			}
+			else
+			{
+				trans.Rollback();
+				return Ok(new ResponseClass() { success = false });
+			}
 		}
 		[HttpGet]
 		public async Task<IHttpActionResult> ActiveLocation(int id)
 		{
-			Location location = await p.Location.FindAsync(id);
+			Location location = await db.Location.FindAsync(id);
 			if (location == null)
 			{
 				return NotFound();
 			}
-			location.IS_Action = true;
-			await p.SaveChangesAsync();
+			var trans = db.Database.BeginTransaction();
 
-			return Ok(new ResponseClass() { success = true });
+			location.IS_Action = true;
+
+			await db.SaveChangesAsync();
+
+
+			var logstate = Logger.AddLog(db: db, logClass: LogClassType.Locations, Method: "Active", Oldval: null, Newval: null, es: out _, syslog: out _, ID: location.Location_ID, notes: "");
+			if (logstate)
+			{
+				await db.SaveChangesAsync();
+				trans.Commit();
+				return Ok(new ResponseClass()
+				{
+					success = true
+				});
+			}
+			else
+			{
+				trans.Rollback();
+				return Ok(new ResponseClass() { success = false });
+			}
 		}
 
 		protected override void Dispose(bool disposing)
 		{
 			if (disposing)
 			{
-				p.Dispose();
+				db.Dispose();
 			}
 			base.Dispose(disposing);
 		}
