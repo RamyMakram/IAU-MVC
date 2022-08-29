b9666c2a1f9d4123a1b942918c301cb784811856
Ramy Makram <RamyMakramEyd@outlook.com>
2022-08-29 13:58:10 +0200

    Add MostaidApp Api Project Files

---
+++ MustafidApp/Controllers/v1/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MustafidApp.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "data from api v1";
+        }
+    }
+}
---
+++ MustafidApp/Controllers/v2/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MustafidApp.Controllers.v2
+{
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "data from api v2";
+        }
+    }
+}
