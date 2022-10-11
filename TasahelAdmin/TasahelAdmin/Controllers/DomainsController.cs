8f7a7319256126532b1fc6669284bc4c4f202a69
Ramy Makram <RamyMakramEyd@outlook.com>
2022-10-11 13:02:17 +0200

    Handel Create

--- TasahelAdmin/TasahelAdmin/Controllers/DomainsController.cs
+++ TasahelAdmin/TasahelAdmin/Controllers/DomainsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.IO;
 using System.Linq;
 using System.Runtime.Intrinsics.Arm;
@@ -34,13 +35,20 @@ namespace TasahelAdmin.Controllers
         {
             return View();
         }
-        public async Task<IActionResult> Edit(int id)
+        public async Task<IActionResult> Delete(int id)
         {
-            var data = await db.Domains.Include(q => q.SubDomains).Include(q => q.DomainEmail).Include(q => q.DomainInfo).Include(q => q.DomainStyle).Include(q => q.TasahelHomeSetting)
-                        .FirstOrDefaultAsync(q => q.Id == id);
-            var data_vm = mapper.Map<DomainCreateVM>(data);
-            return View(data_vm);
+            var data = await db.Domains.FirstOrDefaultAsync(q => q.Id == id);
+            db.Domains.Remove(data);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
+        //public async Task<IActionResult> Edit(int id)
+        //{
+        //    var data = await db.Domains.Include(q => q.SubDomains).Include(q => q.DomainEmail).Include(q => q.DomainInfo).Include(q => q.DomainStyle).Include(q => q.TasahelHomeSetting)
+        //                .FirstOrDefaultAsync(q => q.Id == id);
+        //    var data_vm = mapper.Map<DomainCreateVM>(data);
+        //    return View(data_vm);
+        //}
 
 
         [HttpPost]
@@ -72,9 +80,37 @@ namespace TasahelAdmin.Controllers
                 Directory.CreateDirectory(NewReqpath);
 
 
-                string fileName = Path.GetFileName(dd.DomainStyle._Icon.FileName);
+                string fileName = "";
+
+                if (dd.DomainStyle._Icon != null && dd.DomainStyle._Icon.ValidateImage())
+                {
+                    ModelState.AddModelError("DomainStyle._Icon", "There is Domain With This Url");
+                    trans.Rollback();
+                    return View(dd);
+                }
+                if (dd.DomainStyle._FavIcon != null && dd.DomainStyle._FavIcon.ValidateImage())
+                {
+                    ModelState.AddModelError("DomainStyle._FavIcon", "There is Domain With This Url");
+                    trans.Rollback();
+                    return View(dd);
+                }
+                if (dd.HomeSettings._NewReqICo != null && dd.HomeSettings._NewReqICo.ValidateImage())
+                {
+                    ModelState.AddModelError("DomainStyle._NewReqICo ", "There is Domain With This Url");
+                    trans.Rollback();
+                    return View(dd);
+                }
+                if (dd.HomeSettings._FollowIco != null && dd.HomeSettings._FollowIco.ValidateImage())
+                {
+                    ModelState.AddModelError("HomeSettings._FollowIco", "There is Domain With This Url");
+                    trans.Rollback();
+                    return View(dd);
+                }
 
 
+
+
+                fileName = Path.GetFileName(dd.DomainStyle._Icon.FileName);
                 using (FileStream stream = new FileStream(Path.Combine(Icopath, fileName), FileMode.Create))
                 {
                     dd.DomainStyle._Icon.CopyTo(stream);
@@ -104,7 +140,7 @@ namespace TasahelAdmin.Controllers
                 }
                 await db.SaveChangesAsync();
                 trans.Commit();
-                return RedirectToAction("ManageAbout", new { id = Domain.Id, domainname = Domain.Name });
+                return RedirectToAction("Index", "About", new { id = Domain.Id, domainname = Domain.Name });
             }
             catch (System.Exception ee)
             {
@@ -128,7 +164,14 @@ namespace TasahelAdmin.Controllers
                 Domain.DomainStyle.Favicon = "";
                 Domain.TasahelHomeSetting.FollowIco = "";
                 Domain.TasahelHomeSetting.NewReqIco = "";
-                db.Domains.Update(Domain);
+                db.Attach<Domain>(Domain);
+                db.Entry(Domain).Reference(p => p.DomainStyle).IsModified = false;
+                db.Entry(Domain).Reference(p => p.TasahelHomeSetting).IsModified = false;
+                db.Entry(Domain).Collection(p => p.SubDomains).IsModified = false;
+                db.Entry(Domain).Reference(p => p.DomainEmail).IsModified = false;
+                db.Entry(Domain).Reference(p => p.DomainInfo).IsModified = false;
+                //db.Domains.Update(Domain);
+                db.Entry(Domain).State = EntityState.Modified;
                 await db.SaveChangesAsync();
 
 
@@ -143,41 +186,49 @@ namespace TasahelAdmin.Controllers
                 Directory.CreateDirectory(FollowReqpath);
                 var NewReqpath = Path.Combine(environment.WebRootPath, globalpath, "NewReq", Domain.Domain1);
                 Directory.CreateDirectory(NewReqpath);
-
-
-                string fileName = Path.GetFileName(dd.DomainStyle._Icon.FileName);
-
-
-                using (FileStream stream = new FileStream(Path.Combine(Icopath, fileName), FileMode.Create))
+                string fileName = "";
+                if (dd.DomainStyle._Icon != null)
                 {
-                    dd.DomainStyle._Icon.CopyTo(stream);
-                    Domain.DomainStyle.Icon = Path.Combine(globalpath, "Ico", fileName).Replace("\\", "/");
+                    fileName = Path.GetFileName(dd.DomainStyle._Icon.FileName);
+
+
+                    using (FileStream stream = new FileStream(Path.Combine(Icopath, fileName), FileMode.Create))
+                    {
+                        dd.DomainStyle._Icon.CopyTo(stream);
+                        Domain.DomainStyle.Icon = Path.Combine(globalpath, "Ico", fileName).Replace("\\", "/");
+                    }
+                }
+                if (dd.DomainStyle._FavIcon != null)
+                {
+                    fileName = Path.GetFileName(dd.DomainStyle._FavIcon.FileName);
+                    using (FileStream stream = new FileStream(Path.Combine(Favicopath, fileName), FileMode.Create))
+                    {
+                        dd.DomainStyle._FavIcon.CopyTo(stream);
+                        Domain.DomainStyle.Favicon = Path.Combine(globalpath, "Favico", fileName).Replace("\\", "/");
+                    }
+                }
+                if (dd.HomeSettings._FollowIco != null)
+                {
+                    fileName = Path.GetFileName(dd.HomeSettings._FollowIco.FileName);
+                    using (FileStream stream = new FileStream(Path.Combine(FollowReqpath, fileName), FileMode.Create))
+                    {
+                        dd.HomeSettings._FollowIco.CopyTo(stream);
+                        Domain.TasahelHomeSetting.FollowIco = Path.Combine(globalpath, "FollowReq", fileName).Replace("\\", "/");
+                    }
+                }
+                if (dd.HomeSettings._NewReqICo != null)
+                {
+                    fileName = Path.GetFileName(dd.HomeSettings._NewReqICo.FileName);
+                    using (FileStream stream = new FileStream(Path.Combine(NewReqpath, fileName), FileMode.Create))
+                    {
+                        dd.HomeSettings._NewReqICo.CopyTo(stream);
+                        Domain.TasahelHomeSetting.NewReqIco = Path.Combine(globalpath, "NewReq", fileName).Replace("\\", "/");
+                    }
                 }
 
-                fileName = Path.GetFileName(dd.DomainStyle._FavIcon.FileName);
-                using (FileStream stream = new FileStream(Path.Combine(Favicopath, fileName), FileMode.Create))
-                {
-                    dd.DomainStyle._FavIcon.CopyTo(stream);
-                    Domain.DomainStyle.Favicon = Path.Combine(globalpath, "Favico", fileName).Replace("\\", "/");
-                }
-
-
-                fileName = Path.GetFileName(dd.HomeSettings._FollowIco.FileName);
-                using (FileStream stream = new FileStream(Path.Combine(FollowReqpath, fileName), FileMode.Create))
-                {
-                    dd.HomeSettings._FollowIco.CopyTo(stream);
-                    Domain.TasahelHomeSetting.FollowIco = Path.Combine(globalpath, "FollowReq", fileName).Replace("\\", "/");
-                }
-
-                fileName = Path.GetFileName(dd.HomeSettings._NewReqICo.FileName);
-                using (FileStream stream = new FileStream(Path.Combine(NewReqpath, fileName), FileMode.Create))
-                {
-                    dd.HomeSettings._NewReqICo.CopyTo(stream);
-                    Domain.TasahelHomeSetting.NewReqIco = Path.Combine(globalpath, "NewReq", fileName).Replace("\\", "/");
-                }
                 await db.SaveChangesAsync();
                 trans.Commit();
-                return RedirectToAction("ManageAbout", new { id = Domain.Id, domainname = Domain.Name });
+                return RedirectToAction("Index");
             }
             catch (System.Exception ee)
             {
---
+++ TasahelAdmin/TasahelAdmin/ValidateUploadedImages.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace TasahelAdmin
+{
+    public static class ValidateUploadedImages
+    {
+        public static List<string> images_extentios = new List<string>
+        {
+            ".JPG",
+            ".PNG",
+            ".GIF",
+            ".WEBP",
+            ".TIFF",
+            ".PSD",
+            ".RAW",
+            ".BMP",
+            ".HEIF",
+            ".INDD",
+            ".SVG",
+            ".AI",
+            ".EPS",
+        };
+
+        public static bool ValidateImage(this IFormFile file)
+        {
+            var extention = Path.GetExtension(file.FileName);
+            return images_extentios.Select(q => q.ToLower()).Contains(extention.ToLower());
+        }
+    }
+}
